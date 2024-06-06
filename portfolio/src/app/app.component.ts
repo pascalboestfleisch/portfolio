d5c68aac65e5e056b84b8780234b01cf339b6176
Pascal Boestfleisch <pascalboestfleisch@gmail.com>
2024-06-06 13:45:17 +0200

    added portfolio base structure

---
+++ portfolio/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
+import { ToolsComponent } from './tools/tools.component';
+import { SocialMediaComponent } from './social-media/social-media.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, IntroductionComponent, TopNavigationComponent,
+     ToolsComponent, SocialMediaComponent, ProjectsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'portfolio';
+}
