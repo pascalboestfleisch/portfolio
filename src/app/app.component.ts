ea29d1ad6b26fe5d6834cb52fd335388523092b0
Pascal Boestfleisch <pascalboestfleisch@gmail.com>
2024-07-09 16:19:31 +0200

    deleted projects comonent and fontawesomemodule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,30 +4,17 @@ import { IntroductionComponent } from './introduction/introduction.component';
 import { TopNavigationComponent } from './top-navigation/top-navigation.component';
 import { ToolsComponent } from './tools/tools.component';
 import { SocialMediaComponent } from './social-media/social-media.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { faJava } from '@fortawesome/free-brands-svg-icons';
-import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {far} from '@fortawesome/free-regular-svg-icons';
-import {fas} from '@fortawesome/free-solid-svg-icons';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, IntroductionComponent, TopNavigationComponent,
-     ToolsComponent, SocialMediaComponent, ProjectsComponent, FontAwesomeModule],
+     ToolsComponent, SocialMediaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
   title = 'portfolio';
-  faJava = faJava;
 
-  
-  constructor(
-    library: FaIconLibrary,
- ) {
-   library.addIconPacks(fas, far);
- }
 }
