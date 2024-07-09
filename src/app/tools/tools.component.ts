70103d9521943ec9b0cbafec79111cb7525b9fae
Pascal Boestfleisch <pascalboestfleisch@gmail.com>
2024-07-09 15:54:48 +0200

    imported FontAwesomeModule

--- src/app/tools/tools.component.ts
+++ src/app/tools/tools.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { faJava } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-tools',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './tools.component.html',
   styleUrl: './tools.component.css'
 })
 export class ToolsComponent {
+  faJava = faJava;
 
 }
