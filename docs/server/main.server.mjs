f196b76de40444ef3631621d6cedf018eed6a913
Pascal Boestfleisch <pascalboestfleisch@gmail.com>
2024-07-10 12:06:11 +0200

    new build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-H6S5BRND.mjs";import"./chunk-BUEF6VIX.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-MP3XLDUW.mjs";import"./chunk-BUEF6VIX.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
