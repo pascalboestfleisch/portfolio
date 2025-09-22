ec7f3d930500180b98c7cf253fbd8717665bfec2
Pascal Boestfleisch <pascalboestfleisch@gmail.com>
2025-09-22 11:32:18 +0200

    build changes

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-UJ62SY7D.mjs";import"./chunk-BUEF6VIX.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-BBLRVLQ3.mjs";import"./chunk-BUEF6VIX.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
